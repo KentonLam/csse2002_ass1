007d367c91ce68ab71c2c78ed314f4a7bc025fb8
Kenton Lam <kenton_lam@outlook.com>
2018-08-20 12:22:51 +1000

    Remove star import.

--- src/test/java/GrassBlockTest.java
+++ src/test/java/GrassBlockTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -13,18 +13,18 @@ public class GrassBlockTest {
     @Test
     public void testGetColour() {
         GrassBlock grass = new GrassBlock();
-        assertEquals("Grass is not green.", "green", grass.getColour());
+        Assert.assertEquals("Grass is not green.", "green", grass.getColour());
     }
 
     @Test
     public void testGetBlockType() {
         GrassBlock grass = new GrassBlock();
-        assertEquals("Grass is not grass.", "grass", grass.getBlockType());
+        Assert.assertEquals("Grass is not grass.", "grass", grass.getBlockType());
     }
 
     @Test
     public void testIsCarryable() {
         GrassBlock grass = new GrassBlock();
-        assertFalse("Grass shouldn't be carryable.", grass.isCarryable());
+        Assert.assertFalse("Grass shouldn't be carryable.", grass.isCarryable());
     }
 }
