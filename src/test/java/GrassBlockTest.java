7ca9541d57c0476fd1cde208f4d6a6823c631150
Kenton Lam <kenton_lam@outlook.com>
2018-08-30 21:45:12 +1000

    Use before setup method

--- src/test/java/GrassBlockTest.java
+++ src/test/java/GrassBlockTest.java
@@ -1,30 +1,30 @@
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * GrassBlockTest
  */
 public class GrassBlockTest {
-    @Test
-    public void testConstructor() {
-        GrassBlock grass = new GrassBlock();
+    GrassBlock grass;
+
+    @Before
+    public void setup() {
+        grass = new GrassBlock();
     }
 
     @Test
     public void testGetColour() {
-        GrassBlock grass = new GrassBlock();
         assertEquals("Grass is not green.", "green", grass.getColour());
     }
 
     @Test
     public void testGetBlockType() {
-        GrassBlock grass = new GrassBlock();
         assertEquals("Grass is not grass.", "grass", grass.getBlockType());
     }
 
     @Test
     public void testIsCarryable() {
-        GrassBlock grass = new GrassBlock();
         assertFalse("Grass shouldn't be carryable.", grass.isCarryable());
     }
 }
